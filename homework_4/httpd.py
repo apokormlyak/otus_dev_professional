caa5a5c0b37871f2b2333c78e6f72b12f3815ef5
Alisa Pokormlyak <apokormlyak@yandex.ru>
2024-05-17 08:30:06 +0300

    resolve thread

--- homework_4/httpd.py
+++ homework_4/httpd.py
@@ -14,22 +14,22 @@ def my_server():
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("localhost", 80))
         s.listen(5)
-        print(f"Установлено соединение с {s.getsockname()}")
-        print(f"Сервер {s}")
+        LOGGER.info(f"Установлено соединение с {s.getsockname()}")
+        LOGGER.info(f"Сервер {s}")
         while True:
             conn, addr = s.accept()
-            print(f"con = {conn}")
-            print(f"Соединение от {addr}")
+            LOGGER.info(f"con = {conn}")
+            LOGGER.info(f"Соединение от {addr}")
             data = conn.recv(2048)
             if data:
                 message = data.decode("utf-8")
-                print(f"Получено сообщение: \n {message}")
+                LOGGER.info(f"Получено сообщение: \n {message}")
                 send_data = request_analyzer(message, s)
                 bts = conn.send(str(send_data).encode("utf-8"))
             else:
-                print("Нет сообщения от клиента")
+                LOGGER.info("Нет сообщения от клиента")
                 break
-            print("Закрываю соединение")
+            LOGGER.info("Закрываю соединение")
             conn.close()
 
 
@@ -42,19 +42,18 @@ def request_analyzer(client_message, server):
         status_code = "404 Not Found"
     else:
         message_list = client_message.split("\r\n")
-        print(message_list)
+        LOGGER.info(message_list)
         request_info = message_list[0].split()
         req_method = request_info[0]
         request_source = request_info[1]
         if req_method not in ["GET", "HEAD"]:
             status_code = "405 Method Not Allowed"
-        else:
-            if req_method == "GET":
-                body, status_code, content_len = handle_get_source_request(
-                    request_source
-                )
-            elif req_method == "HEAD":
-                body, status_code, content_len = handle_head_request(request_source)
+        elif req_method == "GET":
+            body, status_code, content_len = handle_get_source_request(
+                request_source
+            )
+        elif req_method == "HEAD":
+            body, status_code, content_len = handle_head_request(request_source)
         content_type = handle_content_type(request_source)
     status_line = f"HTTP/1.1 {status_code}"
     headers = "\r\n".join(
@@ -69,7 +68,7 @@ def request_analyzer(client_message, server):
     )
 
     body = str(body)
-    print("Ответ клиенту")
+    LOGGER.info("Ответ клиенту")
     send_data = "\r\n\r\n".join([headers, body])
 
     return send_data
@@ -124,20 +123,18 @@ def handle_head_request(request_source):
 
 
 def handle_content_type(request_source):
-    if request_source.endswith(".html"):
-        return "text/html"
-    elif request_source.endswith(".css"):
-        return "text/css"
-    elif request_source.endswith(".js"):
-        return "text/javascript"
-    elif request_source.endswith((".jpg", ".jpeg")):
-        return "image/jpeg"
-    elif request_source.endswith(".png"):
-        return "image/png"
-    elif request_source.endswith(".gif"):
-        return "image/gif"
-    elif request_source.endswith(".swf"):
-        return "application/x-shockwave-flash"
+    request_source = request_source.split('.')[-1]
+    content_type_map = {
+        "html": "text/html",
+        "css": "text/css",
+        "js": "text/javascript",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "png": "image/png",
+        "gif": "image/gif",
+        "swf": "application/x-shockwave-flash"
+    }
+    return content_type_map.get(request_source)
 
 
 if __name__ == "__main__":
